6a6a14755a4bdfbcff5e739f291323961751cc63
kenrio <kenken7272721@gmail.com>
2025-01-05 16:19:58 +0900

    Test program of readline function.

--- playground/test_readline.c
+++ playground/test_readline.c
@@ -6,9 +6,31 @@
 /*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/29 15:38:37 by keishii           #+#    #+#             */
-/*   Updated: 2025/01/05 12:10:05 by keishii          ###   ########.fr       */
+/*   Updated: 2025/01/05 16:08:43 by keishii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minishell.h"
+#include <stdlib.h>
+#include <unistd.h>
 
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int	main(void)
+{
+	char	*line;
+
+	rl_outstream = stderr;
+
+	while (1)
+	{
+		line = readline("minishell$ ");
+		if (!line)
+			break ;
+		if (*line)
+			add_history(line);
+		free(line);
+	}
+	exit(0);
+}
