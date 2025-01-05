f167f057ee02d386aacc96cf8a5d11daa69a5daf
kenrio <kenken7272721@gmail.com>
2025-01-05 12:03:43 +0900

    First commit.

---
+++ playground/test_readline.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_readline.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/29 15:38:37 by keishii           #+#    #+#             */
+/*   Updated: 2024/12/29 15:59:49 by keishii          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
