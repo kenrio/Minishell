4ba30f6bf08550f286e5da05e7d236ac852caf0a
kenrio <kenken7272721@gmail.com>
2025-01-05 12:10:46 +0900

    Setup files.

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,14 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_readline.c                                    :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/29 15:38:37 by keishii           #+#    #+#             */
-/*   Updated: 2025/01/05 12:10:05 by keishii          ###   ########.fr       */
+/*   Created: 2025/01/05 12:09:18 by keishii           #+#    #+#             */
+/*   Updated: 2025/01/05 12:10:10 by keishii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minishell.h"
+#include <stdlib.h>
+#include <unistd.h>
 
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
