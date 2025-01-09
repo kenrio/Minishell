f811ffa916028cba3fcb6d39d264217a829bb2ee
kenrio <kenken7272721@gmail.com>
2025-01-09 18:04:53 +0900

    #3 メッセージを良い感じにしました。

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 14:22:39 by keishii           #+#    #+#             */
-/*   Updated: 2025/01/09 16:53:38 by keishii          ###   ########.fr       */
+/*   Updated: 2025/01/09 18:00:44 by keishii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 void	print_message(void)
 {
-	printf("hello world!\n");
+	printf("hello minishell\n");
 }
