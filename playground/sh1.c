f2c56f1a9732ef9235c08e47ebd8e75b952f11c7
kenrio <kenken7272721@gmail.com>
2025-01-31 21:47:37 +0900

    Working on simple shell.

--- playground/sh1.c
+++ playground/sh1.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <signal.h>
 #include <limits.h>
 #include <ctype.h>
 #include <string.h>
 
-# define PROMPT	"prompt$ "
+# define PROMPT	"minishell$ "
 # define LINE_BUF_SIZE	2048
 
-static char	*read_cmd(void);
+// static void	invoke_cmd(void);
 
 struct cmd {
 	int argc;
@@ -22,33 +25,34 @@ struct cmd {
 
 int	main(int argc, char *argv[])
 {
-	char *program_name;
-	char *buf;
-	
+	char		*program_name;
+	char		*buf;
+	pid_t		pid;
+	extern char	**environ;
+
+	(void)argc;
 	program_name = argv[0];
 	printf("program_name: %s\n", program_name);
 	while (1)
 	{
-		// struct cmd	*cmd;
-		fprintf(stdout, "%s", PROMPT);
-		fflush(stdout);
-		buf = read_cmd();
-		printf("main buf: %s\n", buf);
-		if (argc > 0)
+		buf = readline(PROMPT);
+		if (!buf)
+			exit(1);
+		pid = fork();
+		if (pid > 0)
+			waitpid(pid, NULL, 0);
+		else if (pid == 0)
 		{
-			printf("execute command\n");
-			exit(0);
+			if (*buf)
+			{
+				printf("buf: %s\n", buf);
+				execve("/bin/ls", argv, environ);
+			}
+			exit(1);
 		}
+		else
+			exit(1);
 	}
+	free(buf);
 	exit(0);
-}
-
-static char	*read_cmd(void)
-{
-	static char	buf[LINE_BUF_SIZE];
-
-	if (read(1, buf, sizeof buf) < 0)
-		exit(0);
-	printf("read_cmd buf: %s\n", buf);
-	return (buf);
 }
