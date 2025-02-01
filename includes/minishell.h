04f0b685ed8a5a1f9bb7ec2315bb5abdb80a50a1
kenrio <kenken7272721@gmail.com>
2025-02-01 15:48:36 +0900

    Add necessary header files.

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,15 +6,20 @@
 /*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 12:09:18 by keishii           #+#    #+#             */
-/*   Updated: 2025/01/05 14:24:14 by keishii          ###   ########.fr       */
+/*   Updated: 2025/02/01 14:26:38 by keishii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include <unistd.h>
-
+#include <stdlib.h>
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+# define PROMPT "minishell$ "
 
 void	print_message(void);
