4968d61f559b78aeacf106eadbe7ca162531e17a
kenrio <kenken7272721@gmail.com>
2025-02-05 15:26:49 +0900

    Add comments.

--- playground/sh2.c
+++ playground/sh2.c
@@ -28,7 +28,7 @@ struct	buildin {
 };
 
 static void				prompt(void);
-static int				invoke_commands(struct cmd *cmd);
+static int				invoke_commands(struct cmd *cmdhead);
 static void				exec_pipeline(struct cmd *cmd);
 static void				redirect_stdout(char *path);
 static int				wait_pipeline(struct cmd *cmdhead);
@@ -63,7 +63,10 @@ static void prompt(void)
 	fflush(stdout);													// プロンプトが即座に出力されるように、標準出力をフラッシュ
 	if (fgets(buf, LINEBUF_MAX, stdin) == NULL)						// 「LINEBUF_MAX - 1」分のデータを標準入力から読み取り、bufにセットしbufへのポインタを返す
 		exit(0);													// fgets()が失敗し、返り血がNULLの場合、プログラムを終了
-	cmd = parse_command_line(buf);									// 
+
+	cmd = parse_command_line(buf);									// コマンドラインを解析し、パイプorリダイレクトで区切られた各コマンドをcmd構造体のリストとして管理
+																	// 各コマンドの引数はargv[]格納し、NULL終端の配列として扱う									
+
 	if (cmd == NULL)												// cmdがNULLの場合
 	{
 		fprintf(stderr, "%s: syntax error\n", program_name);		// 標準エラーにエラーメッセージを出力
@@ -74,24 +77,79 @@ static void prompt(void)
 	free_cmd(cmd);													// 確保したメモリを解放
 }
 
-static void	invoke_cmd(struct cmd *cmd)
+static int	invoke_commands(struct cmd *cmdhead)
 {
-	pid_t	pid;
+	int	st;															// int型の変数stを宣言
+	int	original_stdin = dup(0);									// 標準入力のfdを複製してoriginal_stdinに代入
+	int	original_stdout = dup(1);									// 標準出力のfdを複製してoriginal_stdoutに代入
 
-	pid = fork();
-	if (pid < 0)
+	exec_pipeline(cmdhead);											// 
+	st = wait_pipeline(cmdhead);
+	close(0); dup2(original_stdin, 0); close(original_stdin);
+	close(1); dup2(original_stdout, 1); close(original_stdout);
+
+	return (st);
+}
+
+#define HEAD_P(cmd) ((cmd) == cmdhead)
+#define TAIL_P(cmd) (((cmd)->next == NULL) || REDIRECT_P((cmd)->next))
+
+static void	exec_pipeline(struct cmd *cmdhead)
+{
+	struct cmd	*cmd;													// cmd構造体を宣言
+	int			fds1[2] = {-1, -1};										// int[2]型の配列fds1を宣言、各要素に-1をセット
+	int			fds2[2] = {-1, -1};										// int[2]型の配列fds2を宣言、各要素に-1をセット
+
+	for (cmd = cmdhead; cmd && !REDIRECT_P(cmd); cmd = cmd->next)		// 
 	{
-		perror("fork");
-		exit(1);
+		fds1[0] = fds2[0];
+		fds1[1] = fds2[1];
+		if (!TAIL_P(cmd))
+		{
+			if (pipe(fds2) < 2)
+			{
+				perror("pipe");
+				exit(3);
+			}
+		}
+		if (lookup_builtin(cmd->argv[0]) != NULL)
+			cmd->pid = PID_BUILTIN;
+		else
+		{
+			cmd->pid = fork();
+			if (cmd->pid < 0)
+			{
+				perror("fork");
+				exit(3);
+			}
+			if (cmd->pid > 0)
+			{
+				if (fds1[0] != -1) close(fds1[0]);
+				if (fds1[1] != -1) close(fds1[1]);
+				continue;
+			}
+		}
+		if (!HEAD_P(cmd))
+		{
+			close(0); dup2(fds1[0], 0); close(fds1[0]);
+			close(fds1[1]);
+		}
+		if (!TAIL_P(cmd))
+		{
+			close(fds2[0]);
+			close(1); dup2(fds2[1], 1); close(fds2[1]);
+		}
+		if ((cmd->next != NULL) && REDIRECT_P(cmd->next))
+			redirect_stdout(cmd->next->argv[0]);
+		if (!BUILTIN_P(cmd))
+		{
+			execvp(cmd->argv[0], cmd->argv);
+			fprintf(stderr, "%s: command not found: %s\n",
+				program_name, cmd->argv[0]);
+			exit(1);
+		}
 	}
-	if (pid > 0)
-		waitpid(pid, NULL, 0);
-	else
-	{
-		execvp(cmd->argv[0], cmd->argv);
-		fprintf(stderr, "%s: command not found: %s\n", program_name, cmd->argv[0]);
-		exit(1);
-	}
+
 }
 
 #define INIT_ARGV 8
@@ -101,18 +159,18 @@ static struct cmd*  parse_command_line(char *p)
 {
 	struct cmd	*cmd;														// cmd構造体へのポインタcmdを宣言
 
-	cmd = xamlloc(sizeof(struct cmd));										// cmd用のメモリをxmallocで確保し、その先頭アドレスをcmdに代入
+	cmd = xmalloc(sizeof(struct cmd));										// cmd用のメモリをxmallocで確保し、その先頭アドレスをcmdに代入
 	cmd->argc = 0;															// argcメンバに0を代入
 	cmd->argv = xmalloc(sizeof(char *) * INIT_ARGV);						// argvメンバ用のメモリをxmallocで確保し、その先頭アドレスをargvに代入
 	cmd->capa = INIT_ARGV;													// capaメンバにINIT_CAPAを代入
 	cmd->next = NULL;														// nextメンバにNULLを代入
 	while (*p)																// pが指す文字がヌル文字（'\0'）でない限り
 	{
-		while (*p && isspace((int)*p))										// pが指す文字がヌル文字（'\0'）でなく、空白文字（スペース、タブ、改行など）の場合
+		while (*p && isspace((int)*p))										// pが指す文字がヌル文字（'\0'）でなく、空白文字（スペース、タブ、改行など）の限り
 			*p++ = '\0';													// pが指す文字をヌル文字（'\0'）にし、ポインタを次の位置に進める
 		if (! IDENT_CHAR_P(*p))												// pが指す文字が空白文字またはパイプ記号またはリダイレクト記号の場合
 			break ;															// whileループを抜ける
-		if (*p && IDENT_CHAR_P(*p))											// pが指す文字がヌル文字（'\0'）でも、空白文字でも、パイプ記号でも、リダイレクト記号でない場合
+		if (*p && IDENT_CHAR_P(*p))											// pが指す文字がヌル文字（'\0'）でも、空白文字でも、パイプ記号でも、リダイレクト記号でもない場合
 		{
 			if (cmd->capa <= cmd->argc)										// capaメンバがargc + 1以下の場合
 			{
@@ -152,6 +210,29 @@ static struct cmd*  parse_command_line(char *p)
 
 static void	free_cmd(struct cmd *cmd)
 {
-	free(cmd->argv);		// cmd構造体のargvメンバのメモリを解放
-	free(cmd);				// cmd構造体のメモリを解放
+	if (cmd->next != NULL)
+		free_cmd(cmd->next);
+	free(cmd->argv);			// cmd構造体のargvメンバのメモリを解放
+	free(cmd);					// cmd構造体のメモリを解放
+}
+
+static void	*malloc(size_t size)
+{
+	void	*p;
+
+	p = calloc(1, size);
+	if (!p)
+		exit(3);
+	return (p);
+}
+
+static void	*xrealloc(void *ptr, size_t size)
+{
+	void	*p;
+
+	if (!ptr) return (xmalloc(size));
+	p = realloc(ptr, size);
+	if (!p)
+		exit(3);
+	return (p);
 }
