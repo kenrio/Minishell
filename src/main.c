e23d5582b763f83f1a25eab303dc9bc9afb13463
kenrio <kenken7272721@gmail.com>
2025-02-23 20:43:19 +0900

    Refactor main loop.

--- src/main.c
+++ src/main.c
@@ -6,38 +6,44 @@
 /*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 12:08:21 by keishii           #+#    #+#             */
-/*   Updated: 2025/02/01 20:10:54 by keishii          ###   ########.fr       */
+/*   Updated: 2025/02/23 18:16:46 by keishii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char *argv[])
+static void	main_loop(char *input_line);
+static char	*get_input_line(void);
+
+int	main(int argc, char **argv, char **envp)
 {
-	char		*program_name;
-	char		*buf;
-	pid_t		pid;
-	extern char	**environ;
+	char	*input_line;
 
 	(void)argc;
-	program_name = argv[0];
-	printf("program_name: %s\n", program_name);
-	while ((buf = readline(PROMPT)))
-	{
-		if (ft_strlen(buf) > 0)
-			add_history(buf);
-		pid = fork();
-		if (pid > 0)
-			waitpid(pid, NULL, 0);
-		else if (pid == 0)
-		{
-			printf("buf: %s\n", buf);
-			execve("/bin/ls", argv, environ);
-			exit(1);
-		}
-		else
-			exit(1);
-	}
-	free(buf);
+	(void)argv;
+	(void)envp;
+	input_line = NULL;
+	main_loop(input_line);
 	exit(0);
+}
+
+static void	main_loop(char *input_line)
+{
+	while (1)
+	{
+		input_line = get_input_line();
+		if (!input_line)
+			break ;
+		free(input_line);
+	}
+}
+
+static char	*get_input_line(void)
+{
+	char	*input_line;
+
+	input_line = readline(PROMPT);
+	if (input_line && ft_strlen(input_line) > 0)
+		add_history(input_line);
+	return (input_line);
 }
