b90912ba1277e19ec6cd89d258bd20f0ba114c8e
kenrio <kenken7272721@gmail.com>
2025-02-25 20:21:36 +0900

    Add ft_isspace() function.

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 14:22:39 by keishii           #+#    #+#             */
-/*   Updated: 2025/01/09 18:00:44 by keishii          ###   ########.fr       */
+/*   Updated: 2025/02/25 15:47:34 by keishii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,9 @@
 void	print_message(void)
 {
 	printf("hello minishell\n");
+}
+
+int	ft_isspace(char c)
+{
+	return (c == ' ' || c == '\t');
 }
