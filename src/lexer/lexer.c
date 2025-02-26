8c43c604e618bf530458e75f5538cf57045580df
kenrio <kenken7272721@gmail.com>
2025-02-26 20:32:21 +0900

    [WIP] Update lexer function. Enable to count tokens.

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,75 +6,74 @@
 /*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 15:01:33 by keishii           #+#    #+#             */
-/*   Updated: 2025/02/25 20:18:14 by keishii          ###   ########.fr       */
+/*   Updated: 2025/02/26 20:28:20 by keishii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 static int	lexer_split(char *input_line, t_token_list *token_list);
+static void	init_lexer(t_token_list *list, t_lexer_flag *flag);
+static void	count_token(char *line, t_token_list *list, t_lexer_flag *flag);
 
-int	lexer(char *input_line, t_token_list *token_list)
+int	lexer(char *input_line)
 {
+	t_token_list	token_list;
+
 	printf("input_line: %s\n", input_line);
-	lexer_split(input_line, token_list);
+	lexer_split(input_line, &token_list);
+	printf("token_list->num: %d\n", token_list.num);
 	return (0);
 }
 
 static int	lexer_split(char *input_line, t_token_list *token_list)
 {
-	int				i;
 	t_lexer_flag	lexer_flag;
 
-	i = 0;
-	lexer_flag.in_squote = false;
-	lexer_flag.in_dquote = false;
-	token_list->num = 0;
-	while (ft_isspace(input_line[i]))
-		i++;
-	printf("input_line[%d]: %c\n", i, input_line[i]);
-	while (input_line[++i])
-	{
-		if (input_line[i] == '\'' && !lexer_flag.in_squote)
-		{
-			printf("input_line[%d]: %c\n", i, input_line[i]);
-			lexer_flag.in_squote = true;
-			i++;
-			while (input_line[i] != '\'')
-			{
-				i++;
-			}
-			printf("input_line[%d]: %c\n", i, input_line[i]);
-			lexer_flag.in_squote = false;
-			token_list->num++;
-			break ;
-		}
-		else if (input_line[i] == '"' && !lexer_flag.in_dquote)
-		{
-			printf("input_line[%d]: %c\n", i, input_line[i]);
-			lexer_flag.in_dquote = true;
-			i++;
-			while (input_line[i] != '"')
-			{
-				i++;
-			}
-			printf("input_line[%d]: %c\n", i, input_line[i]);
-			lexer_flag.in_dquote = false;
-			token_list->num++;
-			break ;
-		}
-		else if (input_line[i] == '|')
-		{
-			token_list->num++;
-		}
-		else if (input_line[i] == ' ')
-		{
-			token_list->num++;
-		}
-	}
-	printf("input_line[%d]: %c\n", i, input_line[i]);
-	printf("lexer_flag.in_squote: %d\n", lexer_flag.in_squote);
-	printf("lexer_flag.in_dquote: %d\n", lexer_flag.in_dquote);
-	printf("token_list->num: %d\n", token_list->num);
+	init_lexer(token_list, &lexer_flag);
+	count_token(input_line, token_list, &lexer_flag);
 	return (0);
+}
+
+static void	init_lexer(t_token_list *list, t_lexer_flag *flag)
+{
+	list->num = 0;
+	flag->in_squote = false;
+	flag->in_dquote = false;
+	flag->new_token = true;
+}
+
+static void	count_token(char *line, t_token_list *list, t_lexer_flag *flag)
+{
+	int	i;
+
+	i = 0;
+	while (ft_isspace(line[i]))
+		i++;
+	if (!line[i])
+		return ;
+	while (line[i])
+	{
+		if (line[i] == '\'' && !(*flag).in_dquote)
+			(*flag).in_squote = !(*flag).in_squote;
+		else if (line[i] == '"' && !(*flag).in_squote)
+			(*flag).in_dquote = !(*flag).in_dquote;
+		if (!(*flag).in_squote && !(*flag).in_dquote)
+		{
+			if (ft_isspace(line[i]))
+				(*flag).new_token = true;
+			else if (line[i] == '|' || line[i] == '<' || line[i] == '>')
+			{
+				if ((*flag).new_token || (i > 0 && (line[i - 1] != line[i])))
+					list->num++;
+				(*flag).new_token = true;
+			}
+			else if ((*flag).new_token)
+			{
+				list->num++;
+				(*flag).new_token = false;
+			}
+		}
+		i++;
+	}
 }
--- src/main.c
+++ src/main.c
@@ -6,36 +6,35 @@
 /*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 12:08:21 by keishii           #+#    #+#             */
-/*   Updated: 2025/02/24 17:17:50 by keishii          ###   ########.fr       */
+/*   Updated: 2025/02/26 18:42:56 by keishii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	main_loop(char *input_line, t_token_list *token_list);
+static void	main_loop(char *input_line);
 static char	*get_input_line(void);
 
 int	main(int argc, char **argv, char **envp)
 {
-	char			*input_line;
-	t_token_list	token_list;
+	char	*input_line;
 
 	(void)argc;
 	(void)argv;
 	(void)envp;
 	input_line = NULL;
-	main_loop(input_line, &token_list);
+	main_loop(input_line);
 	exit(0);
 }
 
-static void	main_loop(char *input_line, t_token_list *token_list)
+static void	main_loop(char *input_line)
 {
 	while (1)
 	{
 		input_line = get_input_line();
 		if (!input_line)
 			break ;
-		lexer(input_line, token_list);
+		lexer(input_line);
 		free(input_line);
 	}
 }
