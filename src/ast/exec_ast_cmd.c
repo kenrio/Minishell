46880ee8ea8534d2e9337412f202116451abfb05
Tsubasa Ishihara <tishihar@c6r2s3.42tokyo.jp>
2025-02-26 14:52:00 +0900

    だいたいast_pipeがわはできた

--- src/ast/exec_ast_cmd.c
+++ src/ast/exec_ast_cmd.c
@@ -6,7 +6,7 @@
 /*   By: tishihar <tishihar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 16:07:58 by tishihar          #+#    #+#             */
-/*   Updated: 2025/02/24 16:16:23 by tishihar         ###   ########.fr       */
+/*   Updated: 2025/02/24 18:42:48 by tishihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,70 @@ static	void	setup_child_fd(int fd_in, int fd_out);
 
 
 
-// ast_cmd_helper
+// exec_ast_cmd() is used as follows.
+// step1. if there is a redirect arrays, replace fd_in and fd_out.
+// step2. fd_in & fd_out to std_in & std_out, use dup().
+// step3. Execute fork(), and execute(). 
 int	exec_ast_cmd(t_ast *ast_node, int fd_in, int fd_out, pid_t *pids)
 {
     // このノードタイプ＝cmdの場合、コマンドを実行する。
-	// 基本はfd_inから読み取り、fd_outに書き込む。リダイレクト処理
+
+	// 1: redirect配列に１つでもredirectが入っていた場合、リダイレクトにfd_in, fd_outを付け替える
+	if (ast_node->data.cmd.redirects != NULL)
+		handle_redirect();
+
+	// null だったらそのまま抜ける
+
+
+	// 2: redirect配列がない場合、単純にfd_inから読み取り、fd_outに書き込めばいい。
+	execute_cmd();
+
+	
+	
+
+
 
 	
 
 	
 }
+
+
+// this func() replace fd_in & fd_out.
+static	int	handle_redirect()
+{
+	
+}
+
+
+// 書き込む必要あるね.いったん入力先と出力先をわたしてみる
+static	void	execute_cmd()
+{
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 static	void	check_fd(int fd_in, int fd_out)
--- src/ast/exec_ast_pipe.c
+++ src/ast/exec_ast_pipe.c
@@ -6,47 +6,108 @@
 /*   By: tishihar <tishihar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 16:04:52 by tishihar          #+#    #+#             */
-/*   Updated: 2025/02/24 16:18:46 by tishihar         ###   ########.fr       */
+/*   Updated: 2025/02/26 14:49:50 by tishihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void close_pipe(int fd_pipe[]);
+static	void 	close_pipe(int fd_pipe[]);
+static	void	wait_left_right();
+static	void	exec_left(t_ast *ast_node, int fd_in, int fd_pipe[], pid_t *pids);
+static	void	exec_right(t_ast *ast_node, int fd_pipe[], int fd_out, pid_t *pids);
 
-// exec_ast_pipe() can only create pipe. this func don't use fork(). 
+
+// pipeでfork()する
 int	exec_ast_pipe(t_ast *ast_node, int fd_in, int fd_out, pid_t *pids)
 {
-    // pipeのとき、パイプを作り、execute_ast(left) &(right)を行う
-    int    fd_pipe[2];
+	int		fd_pipe[2];
 
 	// pipe生み出す。
     if (pipe(fd_pipe) == -1)
+	{
+		perror("crate pipe failed.");
 		return (1);
+	}
+	
+	// pipeがある状態でleftとrightをfork()する。
+	exec_left(ast_node->data.pipe.left, fd_in, fd_pipe, pids);
+	exec_right(ast_node->data.pipe.right, fd_pipe, fd_out, pids);
 
-    // exec(left) & exec(right)
-	if (execute_ast(ast_node->data.pipe.left, fd_in , fd_pipe[1], pids))
-	{
-		// left 実行がエラーだった場合の処理(//TODO fd_pipeいい感じに閉じないとかも)
-		close_pipe(fd_pipe);
-		return (1);
-	}
-	close (fd_pipe[1]);
-	if (execute_ast(ast_node->data.pipe.right, fd_pipe[0], fd_out, pids))
-	{
-		// right 実行がエラーだった場合の処理(//TODO fd_pipeいい感じに閉じないとかも)
-		close_pipe(fd_pipe);
-		return (1);
-	}
-	close(fd_pipe[0]);
+	// 用済みなので速攻でpipeを閉じる
+	close_pipe(fd_pipe);
+	
+	// wait処理
+	wait_left_right();
+
+	// return 
 	return (0);
 }
 
-
-static void close_pipe(int fd_pipe[])
+static	void	close_pipe(int fd_pipe[])
 {
 	if (fd_pipe[0] != -1)
 		close(fd_pipe[0]);
 	if (fd_pipe[1] != -1)	
 		close(fd_pipe[1]);
 }
+
+static	void	wait_left_right()
+{
+	int count;
+
+	count = 2;
+	while (count--)
+		wait(NULL);
+}
+
+static	void	exec_left(t_ast *ast_node, int fd_in, int fd_pipe[], pid_t *pids)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("pipe fork failed.");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		// 要らない方を消す
+		close(fd_pipe[0]);
+
+		// execute_ast();
+		if (execute_ast(ast_node, fd_in, fd_pipe[1], pids))
+		{
+			// 実行がエラーだった場合の処理
+			exit(EXIT_FAILURE);
+		}
+		exit(EXIT_SUCCESS);
+	}
+}
+
+static	void	exec_right(t_ast *ast_node, int fd_pipe[], int fd_out, pid_t *pids)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("pipe fork failed.");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		// 要らない方を消す
+		close(fd_pipe[1]);
+
+		// execute_ast();
+		if (execute_ast(ast_node, fd_pipe[0], fd_out, pids))
+		{
+			// 実行がエラーだった場合の処理
+			exit(EXIT_FAILURE);
+		}
+		exit(EXIT_SUCCESS);
+	}
+}
+
