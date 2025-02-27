1bf1f7e71d2e5a5e9f60614f33e60f47a3fce0c7
Tsubasa Ishihara <tishihar@c6r2s8.42tokyo.jp>
2025-02-27 13:21:42 +0900

    pipe口のcolse対象ばぐってたから変えた

--- src/ast/exec_ast_pipe.c
+++ src/ast/exec_ast_pipe.c
@@ -6,7 +6,7 @@
 /*   By: tishihar <tishihar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 18:31:27 by tishihar          #+#    #+#             */
-/*   Updated: 2025/02/27 13:04:20 by tishihar         ###   ########.fr       */
+/*   Updated: 2025/02/27 13:18:51 by tishihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,10 @@ int	exec_ast_pipe(t_ast *ast_node, int fd_in, pid_t *pids)
 		return (1);
 
 	// close[0]
-	close(fd_pipe[0]);
+	close(fd_pipe[1]);// 書き込み口を消す
 
 	// 右ノードの実行;
-	if (execute_ast(ast_node->data.pipe.right, fd_pipe[0], pids))
+	if (execute_ast(ast_node->data.pipe.right, fd_pipe[0], pids))// 読み取り口が必要
 		return (1);
 
 	return (0);
