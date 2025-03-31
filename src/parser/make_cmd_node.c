12a8b3870482f8ee559cbd7f527ee9cb52d7c922
kenrio <kenken7272721@gmail.com>
2025-03-31 13:01:26 +0900

    [WIP] Concat allocate_argv and set_argv0 to one function.

--- src/parser/make_cmd_node.c
+++ src/parser/make_cmd_node.c
@@ -6,7 +6,7 @@
 /*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 23:44:07 by keishii           #+#    #+#             */
-/*   Updated: 2025/03/29 13:52:33 by keishii          ###   ########.fr       */
+/*   Updated: 2025/03/31 12:57:44 by keishii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,7 @@
 static int	make_empty_cmd_node(t_ast *node, int *exit_status);
 static int	set_cmd_name(t_token_array *array, t_parse_helper *helper, int *exit_status);
 static int	find_cmd_name(t_token_array *array, t_parse_helper *helper, int *exit_status);
-static int	allocate_argv_mem(t_parse_helper *helper, int *exit_status);
-static int	set_argv0(t_ast *node, int *exit_status);
+static int	set_argv0(t_parse_helper *helper, int *exit_status);
 
 int	make_cmd_node(t_token_array *array, t_parse_helper *helper, char **envp, int *exit_status)
 {
@@ -35,16 +34,12 @@ static int	make_empty_cmd_node(t_ast *node, int *exit_status)
 {
 	node->u_data.cmd.name = ft_strdup("");
 	if (!node->u_data.cmd.name)
-	{
-		*exit_status = 1;
-		return (1);
-	}
+		return (*exit_status = 1, 1);
 	node->u_data.cmd.argv = ft_calloc(1, sizeof(char *));
 	if (!node->u_data.cmd.argv)
 	{
 		free(node->u_data.cmd.argv);
-		*exit_status = 1;
-		return (1);
+		return (*exit_status = 1, 1);
 	}
 	node->u_data.cmd.argv[0] = NULL;
 	return (0);
@@ -54,9 +49,7 @@ static int	set_cmd_name(t_token_array *array, t_parse_helper *helper, int *exit_
 {
 	if (find_cmd_name(array, helper, exit_status))
 		return (1);
-	if (allocate_argv_mem(helper, exit_status))
-		return (1);
-	if (set_argv0(helper->node, exit_status))
+	if (set_argv0(helper, exit_status))
 		return (1);
 	helper->index++;
 	return (0);
@@ -79,34 +72,24 @@ static int	find_cmd_name(t_token_array *array, t_parse_helper *helper, int *exit
 		}
 	}
 	if ((!helper->node->u_data.cmd.name))
-	{
-		*exit_status = 1;
-		return (1);
-	}
+		return (*exit_status = 1, 1);
 	return (0);
 }
 
-static int	allocate_argv_mem(t_parse_helper *helper, int *exit_status)
+static int	set_argv0(t_parse_helper *helper, int *exit_status)
 {
 	helper->node->u_data.cmd.argv = ft_calloc(helper->arg_count + 1, sizeof(char *));
 	if (!helper->node->u_data.cmd.argv)
 	{
 		free(helper->node->u_data.cmd.name);
-		*exit_status = 1;
-		return (1);
+		return (*exit_status = 1, 1);
 	}
-	return (0);
-}
-
-static int	set_argv0(t_ast *node, int *exit_status)
-{
-	node->u_data.cmd.argv[0] = ft_strdup(node->u_data.cmd.name);
-	if (!node->u_data.cmd.argv[0])
+	helper->node->u_data.cmd.argv[0] = ft_strdup(helper->node->u_data.cmd.name);
+	if (!helper->node->u_data.cmd.argv[0])
 	{
-		free(node->u_data.cmd.argv);
-		free(node->u_data.cmd.name);
-		*exit_status = 1;
-		return (1);
+		free(helper->node->u_data.cmd.argv);
+		free(helper->node->u_data.cmd.name);
+		return (*exit_status = 1, 1);
 	}
 	return (0);
 }
