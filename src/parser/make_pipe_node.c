0782f3b5449433a5ec0095b7ee9ca2ba62362dad
kenrio <kenken7272721@gmail.com>
2025-04-02 20:11:50 +0900

    Remove all debug code.

--- src/parser/make_pipe_node.c
+++ src/parser/make_pipe_node.c
@@ -6,7 +6,7 @@
 /*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 23:32:34 by keishii           #+#    #+#             */
-/*   Updated: 2025/03/31 19:39:02 by keishii          ###   ########.fr       */
+/*   Updated: 2025/04/02 20:06:31 by keishii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,10 @@ int	make_pipe_node(t_token_array *array, t_parse_helper *helper, t_envl *envl,
 	t_ast	*right_node;
 	t_ast	*original_node;
 
-	printf("DEBUG: make_pipe_node start, index=%d\n", helper->index);
 	left_node = ft_calloc(1, sizeof(t_ast));
 	if (!left_node)
 		return (*exit_status = 1, 1);
 	*left_node = *(helper->node);
-	printf("DEBUG: Left node created and copied\n");
 	helper->node->type = NODE_PIPE;
 	helper->node->u_data.pipe.left = left_node;
 	right_node = ft_calloc(1, sizeof(t_ast));
@@ -33,21 +31,15 @@ int	make_pipe_node(t_token_array *array, t_parse_helper *helper, t_envl *envl,
 		free(left_node);
 		return (*exit_status = 1, 1);
 	}
-	printf("DEBUG: Right node created\n");
 	helper->node->u_data.pipe.right = right_node;
 	original_node = helper->node;
 	helper->node = right_node;
-	printf("DEBUG: Before parse_pipe for right side, index=%d\n", helper->index);
 	if (parse_pipe(array, helper, envl, exit_status))
 	{
-		printf("DEBUG: parse_pipe for right side failed\n");
 		free(helper->node->u_data.pipe.left);
 		free(helper->node->u_data.pipe.right);
 		return (*exit_status = 1, 1);
 	}
-	printf("DEBUG: After parse_pipe for right side, helper->node=%p, right_node=%p\n", 
-		(void*)helper->node, (void*)right_node);
 	helper->node = original_node;
-	printf("DEBUG: make_pipe_node end, helper->node restored to %p\n", (void*)helper->node);
 	return (0);
 }
--- src/parser/parse_pipe.c
+++ src/parser/parse_pipe.c
@@ -6,7 +6,7 @@
 /*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 23:30:31 by keishii           #+#    #+#             */
-/*   Updated: 2025/03/31 19:39:56 by keishii          ###   ########.fr       */
+/*   Updated: 2025/04/02 20:04:53 by keishii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,15 @@
 int	parse_pipe(t_token_array *array, t_parse_helper *helper, t_envl *envl,
 		int *exit_status)
 {
-	printf("DEBUG: parse_pipe start, index=%d\n", helper->index);
 	if (parse_cmd(array, helper, envl, exit_status))
-	{
-		printf("DEBUG: parse_cmd failed\n");
 		return (*exit_status = 1, 1);
-	}
-	printf("DEBUG: After parse_cmd, index=%d, len=%d\n", helper->index, array->len);
 	if (helper->index < array->len
 		&& array->tokens[helper->index].token_type == PIPE)
 	{
-		printf("DEBUG: Found PIPE token at index %d\n", helper->index);
 		helper->index++;
 		if (make_pipe_node(array, helper, envl, exit_status))
 			return (*exit_status = 1, 1);
 		return (0);
 	}
-	printf("DEBUG: parse_pipe end, index=%d\n", helper->index);
 	return (0);
 }
