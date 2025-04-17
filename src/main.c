560ee23241279863ebb2424f60c8d1c5be369e86
kenrio <kenken7272721@gmail.com>
2025-04-17 17:41:25 +0900

    Minor changes.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 12:08:21 by keishii           #+#    #+#             */
-/*   Updated: 2025/04/17 01:17:48 by keishii          ###   ########.fr       */
+/*   Updated: 2025/04/17 17:37:26 by keishii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,10 +41,10 @@ static int	main_loop(char *input_line, t_envl *envl, int *exit_status)
 	while (1)
 	{
 		input_line = get_input_line(exit_status);
-		if (!input_line)
-			break ;
 		if (g_signal != 0)
 			continue ;
+		if (!input_line)
+			break ;
 		*exit_status = 0;
 		lexer(&token_array, input_line, exit_status);
 		if (token_array.len == 0 || *exit_status == 1)
