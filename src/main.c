75a01ccbd898dfe63f64497495f886e208bdb36e
kenrio <kenken7272721@gmail.com>
2025-04-17 19:47:31 +0900

    [WIP] Debug exit_status.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 12:08:21 by keishii           #+#    #+#             */
-/*   Updated: 2025/04/17 18:17:51 by keishii          ###   ########.fr       */
+/*   Updated: 2025/04/17 19:33:03 by keishii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,20 +40,24 @@ static int	main_loop(char *input_line, t_envl *envl, int *exit_status)
 
 	while (1)
 	{
+		// printf("initial: exit_status: %d\n", *exit_status);
 		input_line = get_input_line(exit_status);
 		if (g_signal != 0)
 			continue ;
 		if (!input_line)
 			break ;
 		lexer(&token_array, input_line, exit_status);
-		if (token_array.len == 0 || *exit_status != 0)
+		// printf("lexer: exit_status: %d\n", *exit_status);
+		if (token_array.len == 0 || *exit_status == 1)
 		{
 			free_token_array(&token_array);
 			continue ;
 		}
 		parser(&ast_node, &token_array, envl, exit_status);
+		// printf("parser: exit_status: %d\n", *exit_status);
 		free_token_array(&token_array);
 		run_ast(ast_node, envl, exit_status);
+		// printf("exec: exit_status: %d\n", *exit_status);
 		free_ast(ast_node);
 	}
 	return (*exit_status);
