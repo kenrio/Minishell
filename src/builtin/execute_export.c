36824e70e87343acca27ef2b796f8fde78aab5ad
kenrio <kenken7272721@gmail.com>
2025-04-22 02:33:21 +0900

    Improve builtin export command.
    Add env variable only if input has a '=' character
    after a valid key.

--- src/builtin/execute_export.c
+++ src/builtin/execute_export.c
@@ -6,7 +6,7 @@
 /*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 01:41:22 by keishii           #+#    #+#             */
-/*   Updated: 2025/04/20 23:32:57 by keishii          ###   ########.fr       */
+/*   Updated: 2025/04/22 02:29:47 by keishii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ static bool	is_valid_key(const char *arg);
 int	execute_export(t_ast *ast, t_envl *envl)
 {
 	t_env_node	*curr;
-	char		*new_arg;
 	int			i;
 
 	if (!ast->u_data.cmd.argv[1])
@@ -28,7 +27,7 @@ int	execute_export(t_ast *ast, t_envl *envl)
 			printf("declare -x %s\n", curr->value);
 			curr = curr->next;
 		}
-		return (0);
+		return (*(ast->u_data.cmd.stp) = 0, 0);
 	}
 	i = 1;
 	while (ast->u_data.cmd.argv[i])
@@ -36,20 +35,8 @@ int	execute_export(t_ast *ast, t_envl *envl)
 		if (is_valid_key(ast->u_data.cmd.argv[i]))
 		{
 			if (ft_strchr(ast->u_data.cmd.argv[i], '='))
-			{
 				if (envl_add_node(envl, ast->u_data.cmd.argv[i]) != 0)
 					return (*(ast->u_data.cmd.stp) = 1, 1);
-			}
-			else
-			{
-				new_arg = ft_strjoin(ast->u_data.cmd.argv[i], "=");
-				if (!new_arg || envl_add_node(envl, new_arg) != 0)
-				{
-					free(new_arg);
-					return (*(ast->u_data.cmd.stp) = 1, 1);
-				}
-				free(new_arg);
-			}
 		}
 		else
 		{
