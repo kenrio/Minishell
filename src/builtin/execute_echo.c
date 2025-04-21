862a568b8bcb35e6abdb0424f9a211908fb2afac
kenrio <kenken7272721@gmail.com>
2025-04-22 01:26:22 +0900

    Delete unecessary function.

--- src/builtin/execute_echo.c
+++ src/builtin/execute_echo.c
@@ -6,7 +6,7 @@
 /*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 16:29:27 by tishihar          #+#    #+#             */
-/*   Updated: 2025/04/20 23:28:51 by keishii          ###   ########.fr       */
+/*   Updated: 2025/04/22 01:22:53 by keishii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,17 +38,6 @@ int	execute_echo(t_ast *ast)
 	if (newline)
 		ft_putchar_fd('\n', STDOUT_FILENO);
 	return(*(ast->u_data.cmd.stp) = 0, 0);
-}
-
-static bool is_all_n(const char *arg)
-{
-	while (*arg)
-	{
-		if (*arg != 'n')
-			return (false);
-		arg++;
-	}
-	return (true);
 }
 
 static bool is_all_n(const char *arg)
