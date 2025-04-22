b6dd112ad9390967de2ced0a773c4142495cc71d
kenrio <kenken7272721@gmail.com>
2025-04-22 13:53:46 +0900

    Fix norm errors.

--- src/builtin/execute_echo.c
+++ src/builtin/execute_echo.c
@@ -6,7 +6,7 @@
 /*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 16:29:27 by tishihar          #+#    #+#             */
-/*   Updated: 2025/04/22 01:22:53 by keishii          ###   ########.fr       */
+/*   Updated: 2025/04/22 11:27:50 by keishii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,10 +37,10 @@ int	execute_echo(t_ast *ast)
 	}
 	if (newline)
 		ft_putchar_fd('\n', STDOUT_FILENO);
-	return(*(ast->u_data.cmd.stp) = 0, 0);
+	return (*(ast->u_data.cmd.stp) = 0, 0);
 }
 
-static bool is_all_n(const char *arg)
+static bool	is_all_n(const char *arg)
 {
 	while (*arg)
 	{
